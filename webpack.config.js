cd4c1f9a38973777ebefb4e2898533b5578099a1
Your Name <mikeetree@gmail.com>
2021-01-21 20:02:23 -0500

    Configuration files, npm scripts

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './client/src'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './client/public/build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/build/',
+    contentBase: path.resolve(__dirname, './client/public'),
+  },
+};
