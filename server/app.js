3e036f42f1b663701b035397ae5f4aeaf930656b
Your Name <mikeetree@gmail.com>
2021-01-23 13:02:42 -0500

    Serves client

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
-const app = require('express')();
+const express = require('express');
+const path = require('path');
 const api = require('./api');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/public')));
 
 app.use('/api', api);
 
