c8943ad84bdd8e7d12045d6a0c77075a0b230551
Your Name <mikeetree@gmail.com>
2021-01-24 18:31:19 -0500

    Implement ProductOverview

---
+++ client/src/components/ProductOverview.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Slideshow from './Slideshow';
+import Details from './Details';
+import ProductContext from './ProductContext';
+
+export default function ProductOverview() {
+  const [product, setProduct] = useState(null);
+
+  function getProduct() {
+    axios.get('/api/products/random')
+      .then((res) => {
+        if (res.data) {
+          setProduct(res.data);
+        }
+      });
+  }
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  if (product === null) {
+    return (<div>Loading...</div>);
+  }
+
+  return (
+    <div className="product-overview-container">
+      <ProductContext.Provider value={product}>
+        <Slideshow />
+        <Details />
+      </ProductContext.Provider>
+    </div>
+  );
+}
