7ae8630f9b55d631dbf93d4c0a166e9aa17220d3
Your Name <mikeetree@gmail.com>
2021-01-24 18:56:18 -0500

    Separate db from product model, tests no longer hang

---
+++ database/models/Product.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+let Product;
+
+const productSchema = mongoose.Schema({
+  product_id: {
+    type: Number,
+    validate: {
+      validator(productId) {
+        return Product.findOne({ product_id: productId })
+          .then((r) => (r === null));
+      },
+      message: 'Product Id validation failed, duplicate id found',
+    },
+  },
+  brand_name: String,
+  name: String,
+  description: String,
+  price: {
+    base_price: Number,
+    discount: Number,
+  },
+  image_urls: [String],
+  options: [{
+    color: String,
+    size: String,
+    inventory: Number,
+  }],
+});
+
+Product = mongoose.model('products', productSchema);
+
+const clearProducts = () => Product.deleteMany({});
+
+const addProduct = (product) => {
+  const newProduct = new Product(product);
+  return newProduct.save();
+};
+
+const addProducts = (products) => (
+  Product.insertMany(products)
+);
+
+const getProductById = (productId) => (
+  Product.findOne({ product_id: productId }).exec()
+);
+
+const getAllProducts = () => (
+  Product.find({}).exec()
+);
+
+const getRandomProduct = async () => {
+  const count = await Product.countDocuments({}).exec();
+  const rand = Math.floor(Math.random() * count);
+
+  return Product.findOne({}).skip(rand).exec();
+};
+
+module.exports.addProduct = addProduct;
+module.exports.addProducts = addProducts;
+module.exports.getProductById = getProductById;
+module.exports.getAllProducts = getAllProducts;
+module.exports.getRandomProduct = getRandomProduct;
+module.exports.clearProducts = clearProducts;
--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./api');
+require('../database');
 
 const app = express();
 
