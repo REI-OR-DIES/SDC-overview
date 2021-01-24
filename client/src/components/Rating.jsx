cf7ba45d0c1c4c905177f68a9ce0dd612d9837a9
Your Name <mikeetree@gmail.com>
2021-01-24 18:04:06 -0500

    Implement Rating component

---
+++ client/src/components/Rating.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import ProductContext from './ProductContext';
+
+export default function Rating() {
+  const { rating } = useContext(ProductContext);
+  const starsRounded = Math.floor(rating.stars);
+  const filled = '*'.repeat(starsRounded);
+  const unfilled = '*'.repeat(5 - starsRounded);
+
+  return (
+    <div className="rating-container">
+      <span className="rating-stars filled">{filled}</span>
+      <span className="rating-stars unfilled">{unfilled}</span>
+      <span className="rating-average">{rating.stars}</span>
+      <span className="rating-count">{`(${rating.count})`}</span>
+    </div>
+  );
+}
