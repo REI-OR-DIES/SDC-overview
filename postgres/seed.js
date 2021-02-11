9acb9602d9ff4559cc0ec311a47455ff7a73b576
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-02-10 20:15:13 -0700

    change file name again

--- postgres/seed.js
+++ postgres/seed.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 // create products table from products csv file
 // then add auto-incrementing id column in that table
 // const productsFile = path.join(__dirname, 'products.csv');
-const productsQuery = 'COPY products FROM \'products.csv\' DELIMITER \',\' CSV HEADER;';
+const productsQuery = 'COPY products FROM \'./products.csv\' DELIMITER \',\' CSV HEADER;';
 pool.query(productsQuery)
   .then(() => pool.query('ALTER TABLE products ADD COLUMN products_id BIGSERIAL PRIMARY KEY;'))
   .then((res) => console.log(res))
@@ -22,7 +22,7 @@ pool.query(productsQuery)
 // create images table from images csv file
 // then add auto-incrementing id column in that table
 // const imagesFile = path.join(__dirname, 'images.csv');
-const imagesQuery = 'COPY products FROM \'images.csv\' DELIMITER \',\' CSV HEADER;';
+const imagesQuery = 'COPY products FROM \'./images.csv\' DELIMITER \',\' CSV HEADER;';
 pool.query(imagesQuery)
   .then(() => pool.query('ALTER TABLE images ADD COLUMN image_id BIGSERIAL PRIMARY KEY;'))
   .then((res) => console.log(res))
